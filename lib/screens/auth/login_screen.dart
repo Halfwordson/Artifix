60352739d8634123b281e59f92150a3262b10216
Kristian Halfwordson <kristianhalfwordson@Kristians-MacBook-Air.local>
2025-07-18 21:04:04 +0200

    Uppdaterade login_screen med ny auth-logik

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../services/auth_service.dart';
+import '../../auth/auth_service.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
